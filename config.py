debaa04ef76d04db854c7fb336368850e6a380e2
Dicky_asqaelani26 <havidzgojek@gmail.com>
2024-05-05 22:28:44 +0700

    Upload: Project

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    HOST = str(os.environ.get("DB_HOST"))
+    USER = str(os.environ.get("DB_USER"))
+    PASSWORD = str(os.environ.get("DB_PASSWORD"))
+    DATABASE = str(os.environ.get("DB_DATABASE"))
+
+    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_RECORD_QUERIES = True
